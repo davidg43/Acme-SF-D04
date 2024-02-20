8108bd3afaa012e752958e33954af8c668f3b44d
pabespnar <pabespnar@alum.us.es>
2024-02-20 19:47:43 +0100

    Start R03 student5

---
+++ src/main/java/acme/entities/AuditRecords.java
@@ -0,0 +1,51 @@
+
+package acme.entities;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Pattern;
+
+import org.hibernate.validator.constraints.URL;
+
+import acme.client.data.AbstractEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+
+public class AuditRecords extends AbstractEntity {
+
+	private static final long	serialVersionUID	= 1L;
+
+	@Pattern(regexp = "AU-[0-9]{4}-[0-9]{3}")
+	@NotBlank
+	@Column(unique = true)
+	private String				code;
+
+	@Past
+	@Temporal(TemporalType.TIMESTAMP)
+	@NotBlank
+	private Date				periodInit;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	@NotBlank
+	private Date				periodEnd;
+
+
+	public enum Mark {
+		A_PLUS, A, B, C, F, F_MINUS
+	}
+
+
+	@URL
+	private String link;
+
+}
