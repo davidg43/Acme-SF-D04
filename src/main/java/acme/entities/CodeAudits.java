3505baf0048168ea44204bc3b1e43f40e3c4311e
pabespnar <pabespnar@alum.us.es>
2024-02-21 09:46:50 +0100

    Updated CodeAudits

---
+++ src/main/java/acme/entities/CodeAudits.java
@@ -0,0 +1,56 @@
+
+package acme.entities;
+
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.OneToOne;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Pattern;
+
+import org.checkerframework.common.aliasing.qual.Unique;
+import org.hibernate.validator.constraints.URL;
+import org.yaml.snakeyaml.error.Mark;
+
+import acme.client.data.AbstractEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class CodeAudits extends AbstractEntity {
+
+	private static final long	serialVersionUID	= 1L;
+
+	@NotBlank
+	@Unique
+	@Pattern(regexp = "[A-Z]{1,3}-[0-9]{3}")
+	private String				code;
+
+	@Past
+	@NotBlank
+	private Date				execution;
+
+
+	@NotBlank
+	public enum type {
+		STATIC, DYNAMIC
+	}
+
+
+	@NotBlank
+	@Column(unique = true)
+	private List<String>	correctiveActions;
+
+	@NotBlank
+	@OneToOne
+	private Mark			mark;
+
+	@URL
+	private String			link;
+
+}
