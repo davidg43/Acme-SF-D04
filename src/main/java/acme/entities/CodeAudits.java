6180afec820cd79949bd95c5d34e578f889e1276
pabespnar <pabespnar@alum.us.es>
2024-02-21 12:16:59 +0100

    Corrected requirement

--- src/main/java/acme/entities/CodeAudits.java
+++ src/main/java/acme/entities/CodeAudits.java
@@ -10,7 +10,6 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Pattern;
 
-import org.checkerframework.common.aliasing.qual.Unique;
 import org.hibernate.validator.constraints.URL;
 
 import acme.client.data.AbstractEntity;
@@ -25,7 +24,7 @@ public class CodeAudits extends AbstractEntity {
 	private static final long	serialVersionUID	= 1L;
 
 	@NotBlank
-	@Unique
+	@Column(unique = true)
 	@Pattern(regexp = "[A-Z]{1,3}-[0-9]{3}")
 	private String				code;
 
