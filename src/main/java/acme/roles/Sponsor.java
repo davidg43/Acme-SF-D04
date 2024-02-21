55c322c77d0970862a7f201296ef15398fb0d9f3
Naredo <nachonaredobernardos@gmail.com>
2024-02-21 10:52:09 +0100

    roles

--- src/main/java/acme/roles/Sponsor.java
+++ src/main/java/acme/roles/Sponsor.java
@@ -1,10 +1,16 @@
 
 package acme.roles;
 
+import javax.persistence.Entity;
 import javax.validation.constraints.NotBlank;
 
 import acme.client.data.AbstractRole;
+import lombok.Getter;
+import lombok.Setter;
 
+@Entity
+@Getter
+@Setter
 public class Sponsor extends AbstractRole {
 
 	private static final long	serialVersionUID	= 1L;
