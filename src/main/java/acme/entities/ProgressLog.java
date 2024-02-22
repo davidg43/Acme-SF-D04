36d19f593eea8065071ec4994d7b241b6cecfe3c
luisgp9 <luuisgarc@gmail.com>
2024-02-22 20:26:14 +0100

    Fix Entitie ProgressLog

--- src/main/java/acme/entities/ProgressLog.java
+++ src/main/java/acme/entities/ProgressLog.java
@@ -3,14 +3,14 @@ package acme.entities;
 
 import java.sql.Date;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Pattern;
-import javax.validation.constraints.Positive;
 import javax.validation.constraints.Size;
-
-import org.checkerframework.common.aliasing.qual.Unique;
 
 import acme.client.data.AbstractEntity;
 import lombok.Getter;
@@ -25,21 +25,21 @@ public class ProgressLog extends AbstractEntity {
 
 	@NotBlank
 	@Pattern(regexp = "PG-[A-Z]{1,2}-[0-9]{4}")
-	@Unique
+	@Column(unique = true)
 	private String				recordId;
 
-	@Positive
 	private int					completeness;
 
 	@NotBlank
-	@Size(max = 101, message = "El comentario no puede tener mas de 101 caracteres")
+	@Size(max = 100)
 	private String				comment;
 
+	@Temporal(TemporalType.TIMESTAMP)
 	@Past
 	private Date				registrationMoment;
 
 	@NotBlank
-	@Size(max = 76, message = "El nombre del responsable no pueden tener mas de 76 caracteres")
-	private String				goals;
+	@Size(max = 75)
+	private String				reponsiblePerson;
 
 }
