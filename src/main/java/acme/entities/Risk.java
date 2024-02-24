80d1e059e0570829dbaef869e5fc6fc73d2e067e
Naredo <nachonaredobernardos@gmail.com>
2024-02-24 18:06:01 +0100

    Entidad Risk

---
+++ src/main/java/acme/entities/Risk.java
@@ -0,0 +1,48 @@
+
+package acme.entities;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Positive;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.URL;
+
+import acme.client.data.AbstractEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Risk extends AbstractEntity {
+
+	private static final long	serialVersionUID	= 1L;
+
+	@NotBlank
+	@Pattern(regexp = "R-[0-9]{3}")
+	private String				reference;
+
+	@NotBlank
+	@Past
+	private Date				date;
+
+	@Positive
+	@NotBlank
+	private Double				probability;
+
+	@NotBlank
+	private Double				value;
+
+	@NotBlank
+	@Length(max = 101)
+	private String				description;
+
+	@URL
+	private String				link;
+
+}
