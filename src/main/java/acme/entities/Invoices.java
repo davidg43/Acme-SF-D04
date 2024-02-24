28188b18803c2eab7ce011ce749704888ae7c5ef
Naredo <nachonaredobernardos@gmail.com>
2024-02-24 11:06:22 +0100

    Relacion con invoices

--- src/main/java/acme/entities/Invoices.java
+++ src/main/java/acme/entities/Invoices.java
@@ -5,60 +5,51 @@ import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.ManyToOne;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
-import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Positive;
+import javax.validation.constraints.PositiveOrZero;
 
 import org.hibernate.validator.constraints.URL;
 
 import acme.client.data.AbstractEntity;
-import acme.client.data.datatypes.Money;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
-public class SponsorShips extends AbstractEntity {
-
-	//Falta relacion a project ? 
+public class Invoices extends AbstractEntity {
 
 	private static final long	serialVersionUID	= 1L;
 
 	@NotBlank
 	@Column(unique = true)
-	@Pattern(regexp = "[A-Z]{1,3}-[0-9]{3}")
+	@Pattern(regexp = "IN-[0-9]{4}-[0-9]{4}")
 	private String				code;
 
 	@Past
 	@Temporal(TemporalType.TIMESTAMP)
 	@NotBlank
-	private Date				moment;
+	private Date				registrationTime;
 
-	//@Temporal(SpecDuration)
 	@NotBlank
-	private Date				duration;
+	private Date				due_date;
 
+	@NotBlank
 	@Positive
-	@NotBlank
-	private Money				amount;
+	private Integer				quantity;
 
 	@NotBlank
-	private Type				type;
+	@PositiveOrZero
+	private Integer				tax;
 
-	@Email
-	private String				contact;
+	@NotBlank
+	private Integer				totalAmount;
 
 	@URL
 	private String				link;
-
-	@NotBlank
-	@ManyToOne
-	private Invoices			invoices;
-
 }
