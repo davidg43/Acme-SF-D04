5fbb5a3dce691475e4aec4a1453988641e7b7cb9
Naredo <nachonaredobernardos@gmail.com>
2024-02-24 10:49:40 +0100

    Correciones profesora

--- src/main/java/acme/entities/Invoices.java
+++ src/main/java/acme/entities/Invoices.java
@@ -1,13 +1,18 @@
+
+package acme.entities;
+
 import java.util.Date;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Positive;
 import javax.validation.constraints.PositiveOrZero;
 
-import org.checkerframework.common.aliasing.qual.Unique;
 import org.hibernate.validator.constraints.URL;
 
 import acme.client.data.AbstractEntity;
@@ -22,22 +27,28 @@ public class Invoices extends AbstractEntity {
 	private static final long	serialVersionUID	= 1L;
 
 	@NotBlank
-	@Unique
+	@Column(unique = true)
 	@Pattern(regexp = "IN-[0-9]{4}-[0-9]{4}")
 	private String				code;
 
 	@Past
-	private Date				registration_time;
+	@Temporal(TemporalType.TIMESTAMP)
+	@NotBlank
+	private Date				registrationTime;
 
+	@NotBlank
 	private Date				due_date;
 
+	@NotBlank
 	@Positive
 	private Integer				quantity;
 
+	@NotBlank
 	@PositiveOrZero
 	private Integer				tax;
 
-	private Integer				total_amount;
+	@NotBlank
+	private Integer				totalAmount;
 
 	@URL
 	private String				link;
