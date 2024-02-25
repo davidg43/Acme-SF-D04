05f0edcb05a95aa679c1d247181668b252e6dd94
pabespnar <pabespnar@alum.us.es>
2024-02-26 00:33:30 +0100

    Entity Banner first version

---
+++ src/main/java/acme/entities/Banner.java
@@ -0,0 +1,55 @@
+
+package acme.entities;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.URL;
+
+import acme.client.data.AbstractEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Banner extends AbstractEntity {
+
+	private static final long	serialVersionUID	= 1L;
+
+	@Past
+	@Temporal(TemporalType.DATE)
+	@NotNull
+	private Date				instantiationDate;
+
+	@Past
+	@Temporal(TemporalType.DATE)
+	@NotNull
+	private Date				updateDate;
+
+	@NotNull
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date				periodStart;
+
+	@NotNull
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date				periodEnd;
+
+	@URL
+	@NotNull
+	private String				picture;
+
+	@NotNull
+	@Length(max = 75)
+	private String				slogan;
+
+	@NotNull
+	@URL
+	private String				link;
+}
