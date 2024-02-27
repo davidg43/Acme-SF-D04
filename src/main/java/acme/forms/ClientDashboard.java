8a044c27b65518c42a2963c94c3988308bb23b6a
luisgp9 <luuisgarc@gmail.com>
2024-02-27 20:16:26 +0100

    Update ClientDashboard.java

--- src/main/java/acme/forms/ClientDashboard.java
+++ src/main/java/acme/forms/ClientDashboard.java
@@ -1,7 +1,6 @@
 
 package acme.forms;
 
-import javax.validation.constraints.NotNull;
 import javax.validation.constraints.PositiveOrZero;
 
 import acme.client.data.AbstractForm;
@@ -14,16 +13,12 @@ public class ClientDashboard extends AbstractForm {
 
 	private static final long	serialVersionUID	= 1L;
 
-	@NotNull
 	private int					totalNumberProgressLogsRateLess25;
 
-	@NotNull
 	private int					totalNumberProgressLogsRateBetween25And50;
 
-	@NotNull
 	private int					totalNumberProgressLogsRateBetween50And75;
 
-	@NotNull
 	private int					totalNumberProgressLogsRateMoreThan75;
 
 	@PositiveOrZero
