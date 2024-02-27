2b8420ff9a6d9d419dba512c5e07f483ebb4d409
luisgp9 <luuisgarc@gmail.com>
2024-02-27 20:43:19 +0100

    Create Entitie Notice

---
+++ src/main/java/acme/entities/Notice.java
@@ -0,0 +1,50 @@
+
+package acme.entities;
+
+import java.sql.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.URL;
+
+import acme.client.data.AbstractEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Notice extends AbstractEntity {
+
+	private static final long	serialVersionUID	= 1L;
+
+	@NotNull
+	@Past
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date				instantiationMoment;
+
+	@NotBlank
+	@Length(max = 75)
+	private String				title;
+
+	@NotBlank
+	@Length(max = 75)
+	private String				author;
+
+	@NotBlank
+	@Length(max = 100)
+	private String				message;
+
+	@Email
+	private String				emailAddress;
+
+	@URL
+	private String				link;
+}
