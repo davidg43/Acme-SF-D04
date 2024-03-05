fcddca778681dac2b3f19922f1d82665995a175c
pabespnar <pabespnar@alum.us.es>
2024-03-05 22:07:29 +0100

    R03 - AuditRecords
    AuditRecords.java (falta la condicion del periodo)
    AuditRecords.csv
    Mark

---
+++ src/main/java/acme/entities/AuditRecords.java
@@ -0,0 +1,57 @@
+
+package acme.entities;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Pattern;
+
+import org.hibernate.validator.constraints.URL;
+
+import acme.client.data.AbstractEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+
+public class AuditRecords extends AbstractEntity {
+
+	private static final long	serialVersionUID	= 1L;
+
+	@Pattern(regexp = "AU-[0-9]{4}-[0-9]{3}")
+	@NotBlank
+	@Column(unique = true)
+	private String				code;
+
+	@Past
+	@Temporal(TemporalType.TIMESTAMP)
+	@NotNull
+	private Date				periodInit;
+
+	@Past
+	@Temporal(TemporalType.TIMESTAMP)
+	@NotNull
+	private Date				periodEnd;
+
+	@NotNull
+	private Mark				mark;
+
+	@URL
+	private String				link;
+
+	@NotNull
+	@Valid
+	@ManyToOne
+	private CodeAudits			codeAudits;
+
+}
