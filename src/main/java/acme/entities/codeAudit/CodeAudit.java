7b369aa7643e5d8ec1146ea7d0d77d9d6983876e
pabespnar <pabespnar@alum.us.es>
2024-05-27 13:29:13 +0200

    Index out

--- src/main/java/acme/entities/codeAudit/CodeAudit.java
+++ src/main/java/acme/entities/codeAudit/CodeAudit.java
@@ -5,9 +5,7 @@ import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.Index;
 import javax.persistence.ManyToOne;
-import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.Valid;
@@ -28,9 +26,7 @@ import lombok.Setter;
 @Entity
 @Getter
 @Setter
-@Table(indexes = {
-	@Index(columnList = "auditor_id"), @Index(columnList = "project_id"), @Index(columnList = "code")
-})
+
 public class CodeAudit extends AbstractEntity {
 
 	private static final long	serialVersionUID	= 1L;
